b158e100a6ea4da9263b1712e2b8c36f36167121
Anrajin <an.rajin@gmail.com>
2021-04-29 01:08:15 +0600

    hamburger modified

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -43,6 +43,7 @@
                 $(".site-navigation").css("transition-delay", "0.5s");
                 $(".site-navigation .layer").css("transition-delay", "0.3s");
                 $(".site-navigation .inner").css("transition-delay", "0s");
+                $("#itec-logo").attr("src", "assets/images/logow.png");
             } else {
                 $(".site-navigation").addClass("active");
                 $("body").toggleClass("overflow");
@@ -55,6 +56,7 @@
                     "transition-delay",
                     "0.7s"
                 );
+                $("#itec-logo").attr("src", "assets/images/logo.png");
             }
             $(this).toggleClass("is-opened-navi");
         });
