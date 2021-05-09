aeeff97bc23a1b6f04221ff6eaf9476feb75c000
unknown <rzhossain324@gmail.com>
2021-05-09 12:04:12 +0600

    Video uploaded

--- resources/views/Pages/RPL.blade.php
+++ resources/views/Pages/RPL.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <header class="page-header">
         <div class="video-bg">
-            <video src="{{ asset('assets/videos/video02.mp4') }}" muted loop autoplay></video>
+            <video src="{{ asset('assets/videos/rpl-bg-video.m4v') }}" muted loop autoplay></video>
         </div>
         <!-- end video-bg -->
         <div class="inner">
