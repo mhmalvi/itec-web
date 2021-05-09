fadfa09846aac2a26635491b5b548ae6ea1635e1
QuadQue <dev.quadque@gmail.com>
2021-05-09 15:58:04 +0600

    design modification

--- resources/views/Pages/RPL.blade.php
+++ resources/views/Pages/RPL.blade.php
@@ -9,7 +9,7 @@
         <div class="inner">
             <div class="container">
                 <div class="row">
-                    <div class="col-xs-12 col-md-8">
+                    <div class="col-xs-12 col-lg-9">
                         <h1>RPL</h1>
                         <p class="text-justify">
                             Recognition of Prior Learning (RPL) is an assessment process used by RTOs to evaluate a person’s skills,
@@ -19,7 +19,7 @@
                         </p>
                     </div>
 
-                    <div class="col-xs-12 col-md-4 d-none d-lg-block d-xl-none">
+                    <div class="col-xs-12 col-lg-3 d-none d-lg-block d-xl-block">
                         <div class="border">
                             <div class="p-3">
                                 <h3 class="text-center text-light">GET IN TOUCH</h3>
@@ -88,6 +88,28 @@
     <!-- end text-content-block -->
     <section class="testimonials">
         <div class="container justify-content-center">
+            <div class="row">
+                <div class="col-xs-12 d-block d-sm-none mx-auto">
+                    <div class="border">
+                        <div class="p-3">
+                            <h3 class="text-center text-light">GET IN TOUCH</h3>
+                            <p class="text-center text-light">
+                                <small>for a free skill test</small>
+                            </p>
+                            <form action="">
+                                <div class="form-group">
+                                    <input type="text" name="name" id="" class="form-control my-3" placeholder="Your Name"/>
+                                    <input type="email" name="email" id="" class="form-control my-3" placeholder="Your Email Address"/>
+                                    <input type="text" name="phone" id="" class="form-control my-3" placeholder="You Phone Number"/>
+                                    <textarea name="message" id="" rows="3" class="form-control my-3" placeholder="Message"></textarea>
+                                </div>
+
+                                <button type="submit" class="touch-btn">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="row">
                 <div class="col-12 wow" data-splitting>
                     <h3 class="section-title text-center">OUR RTO PARTNERS OFFERS</h3>
--- resources/views/Pages/course-single.blade.php
+++ resources/views/Pages/course-single.blade.php
@@ -8,8 +8,32 @@
         <!-- end video-bg -->
         <div class="inner">
             <div class="container">
-                <h2 class="section-title text-light">{{ $course->course_code }}</h2>
-                <h2 class="section-title text-light">{{ $course->course_name }}</h2>
+                <div class="row">
+                    <div class="col-xs-12 col-lg-9">
+                        <h2 class="section-title text-light">{{ $course->course_code }}</h2>
+                        <h2 class="section-title text-light">{{ $course->course_name }}</h2>
+                    </div>
+                    <div class="col-xs-12 col-lg-3 d-none d-lg-block d-xl-block">
+                        <div class="border">
+                            <div class="p-3">
+                                <h3 class="text-center text-light">GET IN TOUCH</h3>
+                                <p class="text-center">
+                                    <small>for a free skill test</small>
+                                </p>
+                                <form action="">
+                                    <div class="form-group">
+                                        <input type="text" name="name" id="" class="form-control my-3" placeholder="Your Name"/>
+                                        <input type="email" name="email" id="" class="form-control my-3" placeholder="Your Email Address"/>
+                                        <input type="text" name="phone" id="" class="form-control my-3" placeholder="You Phone Number"/>
+                                        <textarea name="message" id="" rows="3" class="form-control my-3" placeholder="Message"></textarea>
+                                    </div>
+        
+                                    <button type="submit" class="touch-btn">Submit</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <!-- end container -->
         </div>
@@ -75,10 +99,147 @@
             <!-- end row -->
             <div class="row justify-content-center my-4 text-light">
                 <div class="col-md-10">
-                    <h4>Qualification Overview:</h4>
-                    <p>
-                        {!! $course->course_desc !!}
+                    <div id="accordion" class="w-100 pt-5 accordion">
+                        <div class="card rounded-0">
+                          <div class="card-header" id="headingOne">
+                            <h5 class="mb-0">
+                              <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                <h4>Qualification Overview:</h4>
+                              </button>
+                            </h5>
+                          </div>
+                      
+                          <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                            <div class="card-body">
+                                <h5>Qualification Name:</h5>
+                                <p>
+                                    {{$course->course_name}}
+                                </p>
+            
+                                <h5>Qualification Code and Units of Competency:</h5>
+                                <p>
+                                    {{ $course->course_code }}
+                                </p>
+            
+                                <h5>Duration:</h5>
+                                <p>
+                                    Within four weeks of receiving sufficient and complying evidence of competency in all applied for units of competency.
+                                </p>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="card rounded-0">
+                          <div class="card-header" id="headingTwo">
+                            <h5 class="mb-0">
+                                <button class="btn btn-link" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseOne">
+                                    <h4>Let us certify your skills:</h4>
+                                </button>
+                            </h5>
+                          </div>
+                          <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                            <div class="card-body">
+
+                            </div>
+                          </div>
+                        </div>
+                        <div class="card rounded-0">
+                          <div class="card-header" id="headingThree">
+                            <h5 class="mb-0">
+                                <button class="btn btn-link" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseOne">
+                                    <h4>Evidence of learning:</h4>
+                                </button>
+                            </h5>
+                          </div>
+                          <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                            <div class="card-body">
+                                <p>
+                                    You will need to provide a range of evidence to assist your assessor to make a decision about your competence. 
+                                    The types of evidence you may be asked to provide can include:
+                                </p>
+                                <ul>
+                                    <li>ID</li>
+                                    <li>Resume</li>
+                                    <li>Payslips/work contract/ABN if self-employed</li>
+                                    <li>Letter of Employment</li>
+                                    <li>Current employment contracts</li>
+                                    <li>Letters of reference from licensed tradesmen you have worked with.</li>
+                                    <li>Videos or photos of your work.</li>
+                                    <li>Evidence of overseas qualifications.</li>
+                                    <li>Transcripts or assessments frome related previous qualifications.</li>
+                                    <li>Apprenticeship papers.</li>
+                                    <li>Safe Work Method Statements&nbsp;</li>
+                                    <li>Job Safety Analysis'</li>
+                                    <li>Quotes/Invoice and stock orders</li>
+                                    <li>Job Cards</li>
+                                    <li>Maintenance Reports/Service sheets&nbsp;</li>
+                                    <li>Testing and diagnostic sheets</li>
+                                </ul>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="card rounded-0">
+                          <div class="card-header" id="headingFour">
+                            <h5 class="mb-0">
+                              <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                <h4>Ease of getting qualified:</h4>
+                              </button>
+                            </h5>
+                          </div>
+                          <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
+                            <div class="card-body">
+                                <ul>
+                                    <li>You can be certified anywhere in Australia with a nationally recognised qualification</li>
+                                    <li>Have your certification within four weeks of evidence being received</li>
+                                    <li>Gain accreditation from a reputable Registered Training Organisation</li>
+                                    <li>Customer Satisfaction Guarantee</li>
+                                    <li>Get your FREE RPL Quote today!</li>
+                                </ul>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="card rounded-0">
+                          <div class="card-header" id="headingFive">
+                            <h5 class="mb-0">
+                              <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                                <h4>This qualification will be offered by one of the following RTOs:</h4>
+                              </button>
+                            </h5>
+                          </div>
+                          <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion">
+                            <div class="card-body">
+
+                            </div>
+                          </div>
+                        </div>
+                    </div>
+
+                    <p class="pt-5">View Other Qualifications in 
+                        <a href="{{ route('industry', $course->courseIndustry->slug) }}" class="text-danger">
+                            {{$course->courseIndustry->title}}
+                        </a>
                     </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 d-block d-sm-none mx-auto">
+                    <div class="border">
+                        <div class="p-3">
+                            <h3 class="text-center text-light">GET IN TOUCH</h3>
+                            <p class="text-center text-light">
+                                <small>for a free skill test</small>
+                            </p>
+                            <form action="">
+                                <div class="form-group">
+                                    <input type="text" name="name" id="" class="form-control my-3" placeholder="Your Name"/>
+                                    <input type="email" name="email" id="" class="form-control my-3" placeholder="Your Email Address"/>
+                                    <input type="text" name="phone" id="" class="form-control my-3" placeholder="You Phone Number"/>
+                                    <textarea name="message" id="" rows="3" class="form-control my-3" placeholder="Message"></textarea>
+                                </div>
+
+                                <button type="submit" class="touch-btn">Submit</button>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
