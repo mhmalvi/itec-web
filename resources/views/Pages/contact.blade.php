566b0ee0601172f2accfc1026fb8a38762b36c46
QuadQue <dev.quadque@gmail.com>
2021-05-20 14:01:26 +0600

    files added

---
+++ resources/views/Pages/contact.blade.php
@@ -0,0 +1,169 @@
+@extends('layouts.app')
+
+@push('css')
+    <link rel="stylesheet" href="{{ asset('assets/css/jquery.datetimepicker.min.css') }}">
+@endpush
+
+@section('content')
+    <!-- end all-cases-link -->
+    <header class="page-header">
+        <div class="video-bg">
+            <video src="{{ asset('assets/videos/contact.m4v') }}" muted loop autoplay></video>
+        </div>
+        <!-- end video-bg -->
+        <div class="inner">
+            <div class="container">
+                <h1>CONTACT</h1>
+                <p>ITECounsel can help you to get your experience recognised!</p>
+            </div>
+            <!-- end container -->
+        </div>
+        <!-- end inner -->
+    </header>
+    <!-- end page-header -->
+    <section class="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5">
+                    <h3 class="section-title">SAY HELLO <br>TO REACH US</h3>
+                </div>
+                <!-- end col-5 -->
+                <div class="col-lg-4 col-md-6">
+                    <h6>HEAD OFFICE</h6>
+                    <address>
+                        Level 1, 7 Greenfield Pde Bankstown,<br>NSW 2200, Sydney, Australia
+                    </address>
+                    <address>
+                        <p>+1300 535 922</p>
+                        <a href="javascript:void(0)">info@itecounsel.com</a>
+                    </address>
+                </div>
+                <!-- end col-4 -->
+                <div class="col-lg-3 col-md-6">
+                    <h6>OFFSHORE BRANCH</h6>
+                    <address>
+                        Level -7, 8/C, F.R Tower, PanthaPath,<br>Dhaka - 1207, Bangladesh
+                    </address>
+
+                    <address>
+                        <p>+88 01309 001 813</p>
+                        <a href="javascript:void(0)">info@itecounsel.com</a>
+                    </address>
+                </div>
+                <!-- end col-3 -->
+            </div>
+            <div class="card shadow p-0 mb-5 bg-white rounded border-0">
+                <div class="card-body p-0">
+                    <div class="row">
+                        <!-- end col-5 -->
+                        <div class="col-lg-6 pr-5 contact-card-inner">
+                            <form id="contactForm">
+                                <ul id="errors"></ul>
+                                <div class="form-group">
+                                    <input id="name" class="form-control" type="text" name="name"
+                                        placeholder="Enter Your Name">
+                                </div>
+                                <div class="form-group">
+                                    <input id="email" class="form-control" type="email" name="email"
+                                        placeholder="Enter Your Email Address">
+                                </div>
+                                <div class="form-group">
+                                    <input id="phone" class="form-control" type="text" name="phone"
+                                        placeholder="Enter Your Cantact Number">
+                                </div>
+                                <div class="form-group">
+                                    <input id="date" class="form-control" type="text" name="date"
+                                        placeholder="Choose Appointment Date">
+                                </div>
+                                <div class="form-group">
+                                    <textarea id="msg" class="form-control" name="msg" rows="8"
+                                        placeholder="Your Message (255 character max)"></textarea>
+                                </div>
+
+                                <button type="submit" class="btn btn-primary" id="form-submit">Send</button>
+                            </form>
+                            <!-- end form -->
+                        </div>
+                        <div class="col-lg-6 pl-5 contact-card-inner contact-card-right">
+                            <h3 class="section-title wow" data-splitting>
+                                NEED <br>CONSULTANCY?
+                            </h3>
+                            <p class="text-justify pr-3">
+                                We know you have a lot of questions in your minds right now. Do not hesitate to ask us
+                                anything you want to know. We got all the answers for you! Feel free to contact us at any
+                                time.
+                            </p>
+                        </div>
+                        <!-- col-7 -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- end container -->
+    </section>
+    <!-- end blog -->
+    <section class="contact py-0">
+        <div class="container-fluid p-0">
+            <div class="row">
+                <div class="col-lg-6 p-0">
+                    <div class="map">
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3310.8570010205053!2d151.03202011550917!3d-33.91907978064232!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b12bc1b224cf2c5%3A0x11c945bdf4b2d7bf!2sLEVEL%201%2F7%20Greenfield%20Parade%2C%20Bankstown%20NSW%202200%2C%20Australia!5e0!3m2!1sen!2sbd!4v1618304659561!5m2!1sen!2sbd"
+                            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                    </div>
+                    <!-- end map -->
+                </div>
+                <div class="col-lg-6 p-0">
+                    <div class="map">
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d228.24124030753345!2d90.38007109664254!3d23.75237686112746!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b9fe484befeb%3A0xf868db2756385e6e!2sITEC%20BD%20Office!5e0!3m2!1sen!2sbd!4v1618301934233!5m2!1sen!2sbd"
+                            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                    </div>
+                    <!-- end map -->
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+
+@push('js')
+    <script src="{{ asset('assets/js/jquery.datetimepicker.full.min.js') }}"></script>
+    <script>
+        $('#date').datetimepicker({
+            step: 30,
+            allowTimes: [
+                '09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00',
+                '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30',
+                '16:00', '16:30', '17:00', '17:30', '18:00', '18:30'
+            ],
+
+        });
+
+        $("#contactForm").on("submit", function(e) {
+            e.preventDefault();
+
+            $.ajax({
+                url: "{!! route('book.appointment') !!}",
+                method: "POST",
+                data: $(this).serialize(),
+                dataType: "json",
+                beforeSend: function() {
+                    $("#form-submit").text('Sending');
+                },
+                success: function(data) {
+                    if (data.status == 422) {
+                        var errors = "";
+                        data.error.map((err) => {
+                            errors += `<li class="text-danger">${err}</li>`;
+                        })
+                        setTimeout(function() {
+                            $("#errors").html(errors);
+                            $("#form-submit").text('Send');
+                        }, 3000);
+                    }
+                }
+            });
+        })
+
+    </script>
+@endpush
---
+++ resources/views/Pages/migration.blade.php
@@ -0,0 +1,147 @@
+@extends('layouts.app')
+
+@push('meta-description')
+<meta name="description" content="ITEC is a licensed visa and immigration specialist with years of experience assisting people with their migration to Australia. Make an appointment./>
+@endpush
+
+@section('content')
+    <!-- end all-cases-link -->
+    <header class="page-header">
+        <div class="video-bg">
+            <video src="{{ asset('assets/videos/Migration.m4v') }}" muted loop autoplay></video>
+        </div>
+        <!-- end video-bg -->
+        <div class="inner">
+            <div class="container">
+                <h1>MIGRATION</h1>
+                <br>
+                <h1 class="heading-secondary">Professional Australian Migration Services</h1>
+                <br>
+                <p>
+                    
+                    ITEC is the most trusted Australian migration services provider in Sydney, and it will assist you by removing all of the hassle from the process and preparing visa applications on behalf of clients to ensure that you get your visa quickly and easily.
+                </p>
+            </div>
+            <!-- end container -->
+        </div>
+        <!-- end inner -->
+    </header>
+    <!-- end page-header -->
+    <section class="works itec-bg-img migration">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 wow" data-splitting>
+                    <h3 class="section-title text-center">ITECounsel's Migration Process</h3>
+                </div>
+                <!-- end col-5 -->
+            </div>
+            <!-- end row -->
+
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="inner-text" data-aos="zoom-out">
+                        <p class="text-justify wow bounceInUp">
+                            You can sign up for a contract with our licensed migration consultant after our initial consultation (online/email/face-to-face consultation) for Australian migration services. 
+                            We'll take care of it for you and prepare your application accordingly. 
+                            A full collection of application forms and checklists will be sent to you by our migration agent in Sydney.
+                        </p>
+                    </div>
+                </div>   
+                <div class="col-lg-4">
+                    <div class="inner-text" data-aos="zoom-out">
+                        <p class="text-justify wow bounceInUp">
+                            As your case-processor, one dedicated member of staff will be assigned to you. 
+                            Our team will continue to assist you at every phase of the process, including training, qualifications evaluation, and filing with the Department of Immigration and Border Protection.
+                        </p>
+                    </div>
+                </div>   
+                <div class="col-lg-4">
+                    <div class="inner-text" data-aos="zoom-out">
+                        <p class="text-justify wow bounceInUp">
+                            We use sophisticated migration process, this system helps with collecting all necessary information, fill the necessary DIBP Forms and we also provide a safe and confidential portal. 
+                            The portal system allows our clients to safely & securely attach their documents (ready for submission to Department of Immigration). 
+                            The documents are encrypted and maintained in a secure environment.
+                        </p>
+                    </div>
+                </div>   
+            </div>
+        </div>
+    </section>
+    <!-- end works -->
+    <section class="works gallary">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 wow" data-splitting>
+                    <h3 class="section-title">
+                        Immigration Services from <br/> Experienced Migration Agent 
+                    </h3>
+                </div>
+                <!-- end col-5 -->
+                <div class="col-lg-8">
+                    <div class="inner-text mt-0" data-aos="zoom-out">
+                        <p class="text-justify wow bounceInUp">
+                            We've been counselling students for educational Opportunity in Foreign countries.
+                            <br/>
+                            There are a number of reasons why you should hire a migration agent to assist you in the very complex process of securing an Australian visa. 
+                            Below we list common top reasons why hiring a Migration Agent is the best thing you can do when migrating to Australia.
+                        </p>
+                        <ul class="itec-list">
+                            <li>- Talk to one of our best consultants today</li>
+                            <li>- Our experts are able to find new growth</li>
+                            <li>- Find more information our website</li>
+                        </ul>
+                        <p class="text-justify wow bounceInUp">
+                            If you want to migrate to Australia, there has never been a better option than ITEC. We help clients who wish to study, work or settle in Australia, 
+                            have a dream of immigrating to Australia or seek Permanent Residency.
+                        </p>
+                    </div>
+                </div>
+                <!-- end col-7 -->
+            </div>
+    </section>
+    <section class="clients">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 wow" data-splitting>
+                    <h3 class="section-title">Apply Now</h3>
+                </div>
+                <div class="col-lg-7">
+                    <form id="migration-form" name="contact" method="post">
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="name" id="name" autocomplete="off"
+                                placeholder="Enter Your Name" required />
+                        </div>
+                        <!-- end form-group -->
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="email" id="email" autocomplete="off"
+                                placeholder="Your Email Address" required />
+                        </div>
+                        <!-- end form-group -->
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="contact" id="contact"
+                                placeholder="Enter Your Contact Number" autocomplete="off" required />
+                        </div>
+                        <!-- end form-group -->
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="nationality" id="nationality"
+                                placeholder="Enter Your Nationality" autocomplete="off" required />
+                        </div>
+                        <!-- end form-group -->
+                        <div class="form-group">
+                            <select name="qualification" id="qualification" class="form-control">
+                                <option value selected disabled>Select Qualification You Looking For</option>
+                            </select>
+                        </div>
+                        <!-- end form-group -->
+                        <div class="form-group">
+                            <button id="submit" type="submit" name="submit">Submit Now</button>
+                        </div>
+                        <!-- end form-group -->
+                    </form>
+                </div>
+            </div>
+        </div>
+        <!-- end container -->
+    </section>
+    <!-- end clients -->
+@endsection
