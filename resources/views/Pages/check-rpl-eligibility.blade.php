2b9d909e3ecb8b941962f2fcf96788e583f48851
ANrajin <an.rajin@gmail.com>
2021-07-05 17:11:34 +0600

    bg video added

--- resources/views/Pages/check-rpl-eligibility.blade.php
+++ resources/views/Pages/check-rpl-eligibility.blade.php
@@ -4,7 +4,7 @@
     <!-- end all-cases-link -->
     <header class="page-header">
         <div class="video-bg">
-            <video src="{{ asset('assets/videos/blog.m4v') }}" muted loop autoplay></video>
+            <video src="{{ asset('assets/videos/check-rpl.mp4') }}" muted loop autoplay></video>
         </div>
         <!-- end video-bg -->
         <div class="inner p-0">
