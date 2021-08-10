0136ae2fd0209f174c9f8cdf9d951b397d45e0b1
ANrajin <dev.quadque@gmail.com>
2021-08-10 15:56:41 +0600

    updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,6 +38,16 @@
 
     <meta name="ahrefs-site-verification" content="c6df018699d9864e4f9c1521bf9ee15e4f9385ca41191ae25fe13bc86a28c0ec">
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-3RZQYK1QW8"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-3RZQYK1QW8');
+    </script>
+
     @include('layouts.styles')
 </head>
 
