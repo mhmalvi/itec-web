41dd866a6f810e8381379a5a96d55459e5c0b14b
ANrajin <dev.quadque@gmail.com>
2021-11-30 11:26:21 +0600

    img lazyload library added

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -1,6 +1,7 @@
 <!-- JS FILES -->
 <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
 <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('js/lazysizes.min.js') }}"></script>
 <script src="{{ asset('assets/js/swiper.min.js') }}"></script>
 <script src="{{ asset('assets/js/wow.min.js') }}"></script>
 <script src="{{ asset('assets/js/splitting.min.js') }}"></script>
