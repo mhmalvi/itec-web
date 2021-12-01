33b16b83871ce2758b38424ff96254dcd9746e3c
ANrajin <dev.quadque@gmail.com>
2021-12-01 09:38:08 +0600

    login page ui updatd

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,71 +1,86 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>@yield('title') - ITEC</title>
 
-    <title>ITEC | Login</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="assets/images/logo/favicon.png">
 
-    <link href="{{ asset('admin/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('admin/font-awesome/css/font-awesome.css') }}" rel="stylesheet">
+    <!-- page css -->
 
-    <link href="{{ asset('admin/css/animate.css') }}" rel="stylesheet">
-    <link href="{{ asset('admin/css/style.css') }}" rel="stylesheet">
+    <!-- Core css -->
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
 
 </head>
 
-<body class="gray-bg">
-
-    <div class="middle-box text-center loginscreen animated fadeInDown">
-        <div>
-            <div class="d-flex justify-content-center" style="padding: 50px 0px 15px 0px;">
-                <img class="lazyload" data-src="{{ asset('images/logo.webp') }}" alt="" style="max-width: 130px;">
-            </div>
-            <div class="py-3">
-                <h3>Welcome Back!</h3>
-                <p>Login to continue</p>
-            </div>
-
-            @if ($errors->any())
-                <div class="text-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <small>{{ $error }}</small>
-                        @endforeach
-                    </ul>
+<body>
+    <div class="app">
+        <div class="container-fluid p-h-0 p-v-20 bg full-height d-flex" style="background-image: url('{{asset('images/login.png')}}')">
+            <div class="d-flex flex-column justify-content-between w-100">
+                <div class="container d-flex h-100">
+                    <div class="row align-items-center w-100">
+                        <div class="col-md-7 col-lg-5 m-h-auto">
+                            <div class="card shadow-lg">
+                                <div class="card-body">
+                                    <div class="d-flex align-items-center justify-content-between m-b-30">
+                                        <img class="img-fluid" alt="" src="{{asset('qq_logo.png')}}">
+                                        <h2 class="m-b-0">Sign In</h2>
+                                    </div>
+                                    @if ($errors->any())
+                                        <div class="text-danger">
+                                            <ul>
+                                                @foreach ($errors->all() as $error)
+                                                    <small>{{ $error }}</small>
+                                                @endforeach
+                                            </ul>
+                                        </div>
+                                    @endif
+                                    <form action="{{ route('login') }}" method="POST">
+                                        @csrf
+                                        <div class="form-group">
+                                            <label class="font-weight-semibold" for="email">Email:</label>
+                                            <div class="input-affix">
+                                                <i class="prefix-icon anticon anticon-user"></i>
+                                                <input type="text" name="email" class="form-control" id="email" value="{{old('email')}}" placeholder="your email address ..." required autofocus>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="font-weight-semibold" for="password">Password:</label>
+                                            <div class="input-affix m-b-10">
+                                                <i class="prefix-icon anticon anticon-lock"></i>
+                                                <input type="password" name="password" class="form-control" id="password" placeholder="Password" autocomplete="current-password" required>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="d-flex align-items-center justify-content-center">
+                                                <button type="submit" class="btn btn-primary">Sign In</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            @endif
-
-            <form class="m-t" role="form" method="POST" action="{{ route('login') }}">
-                @csrf
-                <div class="form-group">
-                    <input type="email" id="email" class="form-control" name="email" :value="old('email')"
-                        placeholder="Your Email" required autofocus>
-                </div>
-                <div class="form-group">
-                    <input type="password" id="password" class="form-control" name="password"
-                        autocomplete="current-password" placeholder="Your Password" required>
-                </div>
-                <button type="submit" class="btn btn-primary block full-width m-b">Login</button>
-
-                <!--@if (Route::has('password.request')) -->
-                <!--    <a href="{{ route('password.request') }}"><small>Forgot password?</small></a>-->
-                <!-- @endif-->
-
-            </form>
-            <p class="m-t"> <small>QuadQue Technologies ltd &copy; <script>
+                <div class="d-none d-md-flex p-h-40 justify-content-between">
+                    <span class="">&copy; <script>
                         document.write(new Date().getFullYear())
 
-                    </script></small> </p>
+                    </script> Quadque Technologies Ltd</span>
+                </div>
+            </div>
         </div>
     </div>
 
-    <!-- Mainly scripts -->
-    <script src="{{ asset('admin/js/jquery-3.1.1.min.js') }}"></script>
-    <script src="{{ asset('admin/js/popper.min.js') }}"></script>
-    <script src="{{ asset('admin/js/bootstrap.js') }}"></script>
+    
+    <!-- Core Vendors JS -->
+    <script src="{{asset('js/admin/vendors.min.js')}}"></script>
+
+    <!-- Core JS -->
+    <script src="{{asset('js/admin/app.min.js')}}"></script>
 
 </body>
 
