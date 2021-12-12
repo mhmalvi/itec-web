92a9d2c78c0f60d3cd01620b8cc5dd38cdb6b526
ANrajin <dev.quadque@gmail.com>
2021-12-12 13:41:55 +0600

    bootstrap icons added

--- resources/views/admin/layouts/styles.blade.php
+++ resources/views/admin/layouts/styles.blade.php
@@ -1,3 +1,4 @@
 <!-- Core css -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 <link href="{{asset('css/app.css')}}" rel="stylesheet">
 <link href="{{asset('css/custom.css')}}" rel="stylesheet">
