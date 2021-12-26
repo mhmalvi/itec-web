349deb687961c4e9dd2a318680462ad1bf24d65e
ANrajin <dev.quadque@gmail.com>
2021-12-26 11:51:04 +0600

    faqs added

--- resources/views/Pages/faq.blade.php
+++ resources/views/Pages/faq.blade.php
@@ -32,43 +32,116 @@
                         <div class="card">
                             <div class="card-header" id="headingOne">
                             <h5 class="mb-0">
-                                <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                Question One
+                                <button class="btn btn-link" data-toggle="collapse" data-target="#q1" aria-expanded="true" aria-controls="collapseOne">
+                                Is RPL allowed for a PR visa in Australia?
                                 </button>
                             </h5>
                             </div>
 
-                            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                            <div id="q1" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
                             <div class="card-body">
-                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                                RPL is a skills’ assessment to be used for PR Visa within Australia. RPL helps to apply for PR Visa in Australia and thus people get opportunities to relocate to Australia. ICT Skill Assessment i.e., RPL report is one of the steps to submit a PR application in Australia examined by ACS. Therefore, it is required to file an RPL to apply for a PR Visa in Australia.
                             </div>
                             </div>
                         </div>
                         <div class="card">
                             <div class="card-header" id="headingTwo">
                             <h5 class="mb-0">
-                                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                Question Two
+                                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#q2" aria-expanded="false" aria-controls="collapseTwo">
+                                What is the best RPL agency in Australia?
                                 </button>
                             </h5>
                             </div>
-                            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                            <div id="q2" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
                             <div class="card-body">
-                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                                There are several agencies in Australia that provide RPL services for interested people. ITEC is one of the best agencies. It provides a free, no-obligation RPL skills check provided by an experienced ITEC representative. Also, ITEC has a partnership with the best RTO providers.
                             </div>
                             </div>
                         </div>
                         <div class="card">
                             <div class="card-header" id="headingThree">
                             <h5 class="mb-0">
-                                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                Quastion Three
+                                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#q3" aria-expanded="false" aria-controls="collapseThree">
+                                What is RPL for Australia?
                                 </button>
                             </h5>
                             </div>
-                            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                            <div id="q3" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
                             <div class="card-body">
-                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                                RPL signifies ‘Recognition of Prior Learning. In general, RPL helps to convert previous skills, knowledge, and learning of an experienced person. This process can be recognised as part of a new qualification. RPL is also a good method to ensure that you do not waste time learning in areas where you are already skilled.
+                            </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header" id="heading5">
+                            <h5 class="mb-0">
+                                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#q5" aria-expanded="false" aria-controls="collapse5">
+                                Which cities are popular for an RPL course in Australia?
+                                </button>
+                            </h5>
+                            </div>
+                            <div id="q5" class="collapse" aria-labelledby="heading5" data-parent="#accordion">
+                            <div class="card-body">
+                                Recognising your talents by an RPL test can assist you in achieving the career you have always wanted. In Australia, RPL is popular and has huge value. Cities like Tasmania, NSW, Sydney are popular among experienced people to pursue RPL.
+                            </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header" id="heading6">
+                            <h5 class="mb-0">
+                                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#q6" aria-expanded="false" aria-controls="collapse6">
+                                Can you be a chef without a degree in Australia?
+                                </button>
+                            </h5>
+                            </div>
+                            <div id="q6" class="collapse" aria-labelledby="heading6" data-parent="#accordion">
+                            <div class="card-body">
+                                Your answer is yes. You can become an experienced chef without studying; however, you must have working experience, a valid RPL certificate, and then assess skills by a chef.
+                            </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header" id="heading7">
+                            <h5 class="mb-0">
+                                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#q7" aria-expanded="false" aria-controls="collapse7">
+                                What is recognition of prior learning in Australia?
+                                </button>
+                            </h5>
+                            </div>
+                            <div id="q7" class="collapse" aria-labelledby="heading7" data-parent="#accordion">
+                            <div class="card-body">
+                                Recognition of Prior Learning is also popularly known as RPL in short. In general, it helps to convert previous skills, knowledge, and learning of an experienced person. This process can be recognised as part of a new qualification. Recognition of Prior Learning is also a good method to ensure that you do not waste time learning in areas where you already are skilled.
+                            </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header" id="heading8">
+                            <h5 class="mb-0">
+                                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#q8" aria-expanded="false" aria-controls="collapse8">
+                                Why do you need RPL Report for Australian Immigration?
+                                </button>
+                            </h5>
+                            </div>
+                            <div id="q8" class="collapse" aria-labelledby="heading8" data-parent="#accordion">
+                            <div class="card-body">
+                                RPL is a term used to describe the term "Recognition of Prior Learning which is an official report submitted to the Australian Computer Society (ACS) by people who wish to migrate to Australia to begin their ICT related jobs, but do not possess an ICT certification or comes from other IT background.
+Therefore, if you don't have any degree that is related to the field of ICT related profession or field, an RPL is mandatory.
+As you can see, there are a lot of people who wish to move to Australia. This is why some proof is needed prior to submitting an apply to become an immigrant. We need skilled migrants, and RPL is one of the ways to demonstrate this.
+
+                            </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header" id="heading9">
+                            <h5 class="mb-0">
+                                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#q9" aria-expanded="false" aria-controls="collapse9">
+                                What is RPL Report and Why it is required?
+                                </button>
+                            </h5>
+                            </div>
+                            <div id="q9" class="collapse" aria-labelledby="heading9" data-parent="#accordion">
+                            <div class="card-body">
+                                Recognisance of Prior Learning (RPL) is a document that needs to be submitted for submission to the Australian Computer Society (ACS). If you're planning to relocate to Australia and would like to continue with your career path in ICT related fields, however, you don't have an academic qualification in ICT qualifications or have a non-related background.
                             </div>
                             </div>
                         </div>
