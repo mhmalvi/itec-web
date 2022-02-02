12bf6c119a422115b2af61f76daf592cb909258b
Zakaria <jakariablaine120@gmail.com>
2022-02-02 18:30:03 +0600

    Add new landing pages (without description)

---
+++ resources/views/Pages/course_landing_pages/CHC43115.blade.php
@@ -0,0 +1,248 @@
+@extends('layouts.app')
+
+@section('title', "Certificate IV in Disability")
+
+@section('content')
+    <header class="page-header">
+        <div class="video-bg">
+            <video src="videos/video02.mp4" muted loop autoplay></video>
+        </div>
+        <!-- end video-bg -->
+        <div class="inner">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-lg-9">
+                        <h2 class="section-title text-light">CHC43115</h2>
+                        <h2 class="section-title text-light">Certificate IV in Disability</h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <div>
+                            <a href="#" class="check-rpl-link">
+                                Check Your RPL Eligibility
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end container -->
+        </div>
+        <!-- end inner -->
+    </header>
+    <!-- end page-header -->
+    <section class="intro">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 px-4">
+                    <h2>Qualification Description</h2>
+                    <p>
+                        This qualification reflects the role of workers in a range of community settings and clients’ homes, who provide training and support in a manner that empowers people with disabilities to achieve greater levels of independence, self-reliance, community participation and wellbeing. Workers promote a person-centred approach, work without direct supervision and may be required to supervise and/or coordinate a small team.
+                    </p>
+                    <p>
+                        To achieve this qualification, the candidate must have completed at least 120 hours of work as detailed in the Assessment Requirements of the units of competency.
+                    </p>
+                    <p>
+                        <i>
+                            No licensing, legislative, regulatory or certification requirements apply to this qualification at the time of publication.
+                        </i>
+                    </p>
+                </div>
+            </div>
+            <!-- end row -->
+        </div>
+        <!-- end container -->
+    </section>
+    <section class="pb-5 rpl-check-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 d-flex justify-content-center">
+                    <div>
+                        <a href="#" class="check-rpl-link-2">
+                            Check Your RPL Eligibility
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- end text-content-block -->
+    <section class="intro-image">
+        <div class="container">
+            <div class="row">
+                <div class="col-12" data-aos="fade-up">
+                    <figure class="post-image text-center">
+                        <img src="{{ asset('course_images/CHC43115.webp') }}"
+                            alt="Image">
+                    </figure>
+                </div>
+                <!-- end col-12 -->
+            </div>
+            <!-- end row -->
+            <div class="row justify-content-center my-4 text-light">
+                <div class="col-md-10">
+                    <div id="accordion" class="w-100 pt-5 accordion" data-aos="fade-up">
+                        <div class="card rounded-0">
+                          <div class="card-header" id="headingOne">
+                            <h5 class="mb-0">
+                              <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                <h4>Qualification Overview:</h4>
+                              </button>
+                            </h5>
+                          </div>
+
+                          <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                            <div class="card-body">
+                                <h5>Qualification Name:</h5>
+                                <p>
+                                    CHC43315<br />
+                                    Certificate IV in Disability
+                                </p>
+
+                                <h5>Qualification Code and Units of Competency:</h5>
+                                <p>
+                                    CHC43115
+                                </p>
+
+                                <h5>Duration:</h5>
+                                <p>
+                                    Within four weeks of receiving sufficient and complying evidence of competency in all applied for units of competency.
+                                </p>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="card rounded-0">
+                          <div class="card-header" id="headingTwo">
+                            <h5 class="mb-0">
+                                <button class="btn btn-link" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseOne">
+                                    <h4>Let us certify your skills:</h4>
+                                </button>
+                            </h5>
+                          </div>
+                          <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                            <div class="card-body">
+
+                            </div>
+                          </div>
+                        </div>
+                        <div class="card rounded-0">
+                          <div class="card-header" id="headingThree">
+                            <h5 class="mb-0">
+                                <button class="btn btn-link" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseOne">
+                                    <h4>Evidence of learning:</h4>
+                                </button>
+                            </h5>
+                          </div>
+                          <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                            <div class="card-body">
+                                <p>
+                                    You will need to provide a range of evidence to assist your assessor to make a decision about your competence.
+                                    The types of evidence you may be asked to provide can include:
+                                </p>
+                                <ul>
+                                    <li>ID</li>
+                                    <li>Resume</li>
+                                    <li>Payslips/work contract/ABN if self-employed</li>
+                                    <li>Letter of Employment</li>
+                                    <li>Current employment contracts</li>
+                                    <li>Letters of reference from licensed tradesmen you have worked with.</li>
+                                    <li>Videos or photos of your work.</li>
+                                    <li>Evidence of overseas qualifications.</li>
+                                    <li>Transcripts or assessments frome related previous qualifications.</li>
+                                    <li>Apprenticeship papers.</li>
+                                    <li>Safe Work Method Statements&nbsp;</li>
+                                    <li>Job Safety Analysis'</li>
+                                    <li>Quotes/Invoice and stock orders</li>
+                                    <li>Job Cards</li>
+                                    <li>Maintenance Reports/Service sheets&nbsp;</li>
+                                    <li>Testing and diagnostic sheets</li>
+                                </ul>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="card rounded-0">
+                          <div class="card-header" id="headingFour">
+                            <h5 class="mb-0">
+                              <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                <h4>Ease of getting qualified:</h4>
+                              </button>
+                            </h5>
+                          </div>
+                          <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
+                            <div class="card-body">
+                                <ul>
+                                    <li>You can be certified anywhere in Australia with a nationally recognised qualification</li>
+                                    <li>Have your certification within four weeks of evidence being received</li>
+                                    <li>Gain accreditation from a reputable Registered Training Organisation</li>
+                                    <li>Customer Satisfaction Guarantee</li>
+                                    <li>Get your FREE RPL Quote today!</li>
+                                </ul>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="card rounded-0">
+                          <div class="card-header" id="headingFive">
+                            <h5 class="mb-0">
+                              <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                                <h4>This qualification will be offered by one of the following RTOs:</h4>
+                              </button>
+                            </h5>
+                          </div>
+                          <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion">
+                            <div class="card-body">
+                                <p class="p-4">RTO</p>
+                            </div>
+                          </div>
+                        </div>
+                    </div>
+
+                    {{-- <p class="pt-5">View Other Qualifications in
+                        <a href="{{ route('industry', $course->courseIndustry->slug) }}" class="text-danger">
+                            {{$course->courseIndustry->title}}
+                        </a>
+                    </p> --}}
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 d-block d-sm-none mx-auto">
+                    <div class="border">
+                        <div class="p-3">
+                            <h3 class="text-center text-light">GET IN TOUCH</h3>
+                            <p class="text-center text-light">
+                                <small>for a free skill test</small>
+                            </p>
+                            <form action="">
+                                <div class="form-group">
+                                    <input type="text" name="name" id="" class="form-control my-3" placeholder="Your Name"/>
+                                    <input type="email" name="email" id="" class="form-control my-3" placeholder="Your Email Address"/>
+                                    <input type="text" name="phone" id="" class="form-control my-3" placeholder="You Phone Number"/>
+                                    <textarea name="message" id="" rows="3" class="form-control my-3" placeholder="Message"></textarea>
+                                </div>
+
+                                <button type="submit" class="touch-btn">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- end container -->
+    </section>
+    <!-- end intro-image -->
+    <section class="clients bg-gray">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 wow" data-splitting>
+                    <h3 class="section-title">
+                        <a href="">CLICK HERE</a> TO DOWNLOAD THE CHECK
+                        LIST
+                    </h3>
+                </div>
+            </div>
+            <!-- end row -->
+        </div>
+        <!-- end container -->
+    </section>
+    <!-- end clients -->
+@endsection
+
