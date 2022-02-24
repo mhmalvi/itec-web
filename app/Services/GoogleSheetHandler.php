7128993d734015b1da8140adbb66962b032f9fdf
Zakaria <jakariablaine120@gmail.com>
2022-02-24 17:48:28 +0600

    Update google sheet's url

--- app/Services/GoogleSheetHandler.php
+++ app/Services/GoogleSheetHandler.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Http;
 class GoogleSheetHandler
 {
     private $data = [];
-    private const RPL_URL = "https://script.google.com/macros/s/AKfycbwBi8QRMS2PUx-lj4YWj_aHIgWVcpvCZAxSkRkSVj46pHWFYxg_VQKjvYfOp9YCq9a6/exec";
+    private const RPL_URL = "https://script.google.com/macros/s/AKfycbzKxuqTCD4jKT65GlHX1zywCfZXyHW9WFdOVU9WCA_2OMeG9FZPCWY04VxuIrIf2ztq-g/exec";
 
     public function setData($data)
     {
@@ -17,6 +17,7 @@ class GoogleSheetHandler
 
     public function saveRPL()
     {
+        $this->data['sheet'] = "ITEC";
         $res = Http::post(self::RPL_URL, $this->data);
 
         if ($res->getStatusCode() != 200) {
